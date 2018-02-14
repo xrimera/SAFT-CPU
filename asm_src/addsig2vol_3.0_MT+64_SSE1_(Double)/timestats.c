1650c246ae41d34f097a8b074d3fbe9ed9339d17
xrimera <xrimera@kit.edu>
2018-02-14 16:39:22 +0100

    timestats updates

--- asm_src/addsig2vol_3.0_MT+64_SSE1_(Double)/timestats.c
+++ asm_src/addsig2vol_3.0_MT+64_SSE1_(Double)/timestats.c
@@ -16,30 +16,32 @@ typedef struct{
     struct timeval result;
 } timer;
 
-
-
-
-
 static timer timers[TS_TIMERS_N] = { 0 };
 
-
-//NOTE: clock() measures time the CPU is executing instruction belonging to this binary. Sleep/Wait doesn't perform any work;
+//NOTE:
+// clock() measures time the CPU is executing instruction belonging to this binary. Sleep/Wait doesn't perform any work;
 // time() measures wallclock time
 void tsclock(unsigned int number){
     #ifdef TS_ON
-    timer* instance = &(timers[number]);
-    if(instance->ticking){
-        gettimeofday(&(instance->toc), NULL);
-        //timers[number].diffsum += abs(measured - timers[number].last);
-        //timers[number].last = measured;
-        timersub(&(instance->toc), &(instance->tic), &(instance->result));
-        instance->sum += instance->result.tv_sec*1000000 + instance->result.tv_usec;
-        instance->num ++;
-    }
-    else{
-        gettimeofday(&(instance->tic), NULL);
-    }
-    instance->ticking = !(instance->ticking);
+        #ifdef TS_THREADSAFE
+            pthread_mutex_lock(&threadsafeMutex);
+        #endif
+        timer* instance = &(timers[number]);
+        if(instance->ticking){
+            gettimeofday(&(instance->toc), NULL);
+            //timers[number].diffsum += abs(measured - timers[number].last);
+            //timers[number].last = measured;
+            timersub(&(instance->toc), &(instance->tic), &(instance->result));
+            instance->sum += instance->result.tv_sec*1000000 + instance->result.tv_usec;
+            instance->num ++;
+        }
+        else{
+            gettimeofday(&(instance->tic), NULL);
+        }
+        instance->ticking = !(instance->ticking);
+        #ifdef TS_THREADSAFE
+            pthread_mutex_unlock(&threadsafeMutex);
+        #endif
     #endif
 }
 
@@ -67,20 +69,48 @@ void tsprint(unsigned int number, double timescale){
 
 void tsclear(unsigned int number){
     #ifdef TS_ON
-    timers[number].sum = 0;
-    timers[number].diffsum = 0;
-    timers[number].last = 0;
-    timers[number].num = 0;
-    timers[number].ticking = 0;
+        #ifdef TS_THREADSAFE
+            pthread_mutex_lock(&threadsafeMutex);
+        #endif
+        timers[number].sum = 0;
+        timers[number].diffsum = 0;
+        timers[number].last = 0;
+        timers[number].num = 0;
+        timers[number].ticking = 0;
+        #ifdef TS_THREADSAFE
+            pthread_mutex_unlock(&threadsafeMutex);
+        #endif
     #endif
 }
 
 void tsclearAll(){
     #ifdef TS_ON
-    char* memory = &timers;
-    for(int i=0; i<TS_TIMERS_N*sizeof(timer); i++){
-        *memory = 0;
-        memory++;
-    }
+        #ifdef TS_THREADSAFE
+            pthread_mutex_lock(&threadsafeMutex);
+        #endif
+        for(int i=0; i<TS_TIMERS_N; i++){
+            timers[number].sum = 0;
+            timers[number].diffsum = 0;
+            timers[number].last = 0;
+            timers[number].num = 0;
+            timers[number].ticking = 0;
+        }
+        #ifdef TS_THREADSAFE
+            pthread_mutex_unlock(&threadsafeMutex);
+        #endif
     #endif
+}
+
+double getAverageTime(unsigned int number, double timescale){
+    #ifdef TS_ON
+        return ((double) timers[number].sum) / timers[number].num * timescale;
+    #endif
+    return .0;
+}
+
+double getSumTime(unsigned int number, double timescale){
+    #ifdef TS_ON
+        return ((double) timers[number].sum) * timescale;
+    #endif
+    return .0;
 }
